6c85053ee7bc33c4b1bc7259e9018edbaea931da
wisdomstar94 <wisdomstar94@gmail.com>
2024-05-01 14:09:56 +0900

    [New Architecture] turbo module, fabric native component 정상 작동하는거 확인. (realm 부분은 에러나서 주석처리)

--- src/screens/TestScreen.tsx
+++ src/screens/TestScreen.tsx
@@ -8,90 +8,91 @@ import { UpdateMode } from 'realm';
 // import dayjs from 'dayjs';
 import RTNCalculator from 'rtn-calculator/js/NativeRtnCalculator';
 import RTNDeviceinfo from 'rtn-deviceinfo/js/NativeRtnDeviceinfo';
+import RTNLottieView from 'rtn-lottie-view/js/RtnLottieViewNativeComponent';
 
 export function TestScreen() {
   const [sumNumber, setSumNumber] = useState(0);
   const [deviceModelName, setDeviceModelName] = useState('null');
 
-  const realmManager = useRealmManager({
-    schema: [User, Log],
-    onChange(modelName, items, changes) {
-      // console.log(`############## useRealmManager.onChange Start ##############`);
-      // console.log('@modelName', modelName);
-      // console.log('@items', items);
-      // console.log('@changes', changes);
-      // changes.deletions.forEach((index) => {
-      //   // You cannot directly access deleted objects,
-      //   // but you can update a UI list, etc. based on the index.
-      //   console.log(`Looks like Dog #${index} has left the realm.`);
-      // });
-      // // Handle newly added Dog objects
-      // changes.insertions.forEach((index) => {
-      //   const insertedDog = items[index];
-      //   console.log(`Welcome our new friend, ${JSON.stringify(insertedDog)}!`);
-      // });
-      // // Handle Dog objects that were modified
-      // changes.newModifications.forEach((index) => {
-      //   const modifiedDog = items[index];
-      //   console.log(`Hey ${JSON.stringify(modifiedDog)}, you look different!`);
-      // });
-    },
-  });
+  // const realmManager = useRealmManager({
+  //   schema: [User, Log],
+  //   onChange(modelName, items, changes) {
+  //     // console.log(`############## useRealmManager.onChange Start ##############`);
+  //     // console.log('@modelName', modelName);
+  //     // console.log('@items', items);
+  //     // console.log('@changes', changes);
+  //     // changes.deletions.forEach((index) => {
+  //     //   // You cannot directly access deleted objects,
+  //     //   // but you can update a UI list, etc. based on the index.
+  //     //   console.log(`Looks like Dog #${index} has left the realm.`);
+  //     // });
+  //     // // Handle newly added Dog objects
+  //     // changes.insertions.forEach((index) => {
+  //     //   const insertedDog = items[index];
+  //     //   console.log(`Welcome our new friend, ${JSON.stringify(insertedDog)}!`);
+  //     // });
+  //     // // Handle Dog objects that were modified
+  //     // changes.newModifications.forEach((index) => {
+  //     //   const modifiedDog = items[index];
+  //     //   console.log(`Hey ${JSON.stringify(modifiedDog)}, you look different!`);
+  //     // });
+  //   },
+  // });
 
-  useEffect(() => {
-    if (!realmManager.isInit) return;
+  // useEffect(() => {
+  //   if (!realmManager.isInit) return;
 
-    // write
-    realmManager.write((realm) => {
-      realm.create<User>(User, {
-        _id: uuid.v4().toString(),
-        name: '홍길동',
-        status: 'active',
-      });
-      realm.create<Log>(Log, {
-        _id: uuid.v4().toString(),
-        title: '로그 1 제목',
-        content: '로그 1 내용',
-        createdAt: Date.now(),
-      });
-    });
+  //   // write
+  //   realmManager.write((realm) => {
+  //     realm.create<User>(User, {
+  //       _id: uuid.v4().toString(),
+  //       name: '홍길동',
+  //       status: 'active',
+  //     });
+  //     realm.create<Log>(Log, {
+  //       _id: uuid.v4().toString(),
+  //       title: '로그 1 제목',
+  //       content: '로그 1 내용',
+  //       createdAt: Date.now(),
+  //     });
+  //   });
 
-    // find all
-    const findAllItems = realmManager.findAll<Log>("Log");
-    console.log('@findAllItems', findAllItems);
+  //   // find all
+  //   const findAllItems = realmManager.findAll<Log>("Log");
+  //   console.log('@findAllItems', findAllItems);
 
-    // modify
-    realmManager.write((realm) => {
-      const findItem = realmManager.findForPrimaryKey<Log>("Log", "d45ee2ba-a77c-4436-af4f-6dac95f72c61");
-      console.log('');
-      console.log('@@modify.findItem', findItem);
-      console.log('');
+  //   // modify
+  //   realmManager.write((realm) => {
+  //     const findItem = realmManager.findForPrimaryKey<Log>("Log", "d45ee2ba-a77c-4436-af4f-6dac95f72c61");
+  //     console.log('');
+  //     console.log('@@modify.findItem', findItem);
+  //     console.log('');
 
-      if (findItem !== undefined) {
-        realm.create<Log>(Log, {
-          _id: "d45ee2ba-a77c-4436-af4f-6dac95f72c61",
-          title: '로그 1 제목 (수정)',
-          content: '로그 1 내용 (수정)',
-          createdAt: findItem.createdAt,
-        }, UpdateMode.Modified);
-      }
-    });
+  //     if (findItem !== undefined) {
+  //       realm.create<Log>(Log, {
+  //         _id: "d45ee2ba-a77c-4436-af4f-6dac95f72c61",
+  //         title: '로그 1 제목 (수정)',
+  //         content: '로그 1 내용 (수정)',
+  //         createdAt: findItem.createdAt,
+  //       }, UpdateMode.Modified);
+  //     }
+  //   });
 
-    // find for primary key
-    const findItem = realmManager.findForPrimaryKey<Log>("Log", "d45ee2ba-a77c-4436-af4f-6dac95f72c61");
-    console.log('@findItem', findItem);
+  //   // find for primary key
+  //   const findItem = realmManager.findForPrimaryKey<Log>("Log", "d45ee2ba-a77c-4436-af4f-6dac95f72c61");
+  //   console.log('@findItem', findItem);
 
-    // delete all
-    realmManager.deleteAll('User');
+  //   // delete all
+  //   realmManager.deleteAll('User');
 
-    // delete
-    realmManager.write((realm) => {
-      // const logs = realm.objects<Log>('Log');
-      const logs = realm.objects<Log>('Log').filter(x => x.createdAt <= 1714038398975);
-      console.log('@delete.logs', logs);
-      realm.delete(logs);
-    });
-  }, [realmManager.isInit]);
+  //   // delete
+  //   realmManager.write((realm) => {
+  //     // const logs = realm.objects<Log>('Log');
+  //     const logs = realm.objects<Log>('Log').filter(x => x.createdAt <= 1714038398975);
+  //     console.log('@delete.logs', logs);
+  //     realm.delete(logs);
+  //   });
+  // }, [realmManager.isInit]);
 
   useEffect(() => {
     RTNCalculator?.add(10, 11).then((result) => {
@@ -119,6 +120,20 @@ export function TestScreen() {
         }}>
         <Text>Test Screen 입니다. { sumNumber }</Text>
         <Text>{ deviceModelName }</Text>
+        <RTNLottieView 
+          style={{ 
+            width: '50%', 
+            height: 100, 
+            backgroundColor: '#ff0', 
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'row',
+            alignContent: 'center',
+            alignSelf: 'center',
+          }} text={deviceModelName + '...2'}
+          />
+        <Text>...</Text>
       </View>
     </>
   );
