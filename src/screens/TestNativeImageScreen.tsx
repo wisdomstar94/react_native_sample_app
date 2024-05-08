fb021c1fd5cee3ef6b7efdc2c343bf25313bef75
wisdomstar94 <wisdomstar94@gmail.com>
2024-05-08 10:57:20 +0900

    rtn_my_library.requestGalleryImage 테스트 (ios 도 정상 작동 하는거 확인 완료..)

--- src/screens/TestNativeImageScreen.tsx
+++ src/screens/TestNativeImageScreen.tsx
@@ -7,30 +7,45 @@ export function TestNativeImageScreen() {
   const [uri, setUri] = useState<string>();
 
   useEffect(() => {
-    library?.requestGalleryImage().then((result) => {
-      const uri = result.trim();
-      if (uri === '') {
-        console.log('@uri 못가져옴...');
-        return;
-      }
-      console.log('@uri', uri);
-      setUri(uri);
-
-      console.log('fetch...');
-      fetch(`file://${uri}`).then(res => {
-        return res.blob();
-      }).then((blob) => {
-        console.log('@blob', blob);
-        const name: string = (blob as any)._data.name;
-        const type: string = (blob as any)._data.type;
-        const lastModified: number = (blob as any)._data.lastModified;
-        console.log('@name', name);
-        const file = new File([blob], name, { type, lastModified });
-        console.log('@file', file);
-      }).catch((error) => {
-        console.error('error', error);
+    setTimeout(() => {
+      library?.getDeviceModel().then((result) => {
+        console.log('library?.getDeviceModel() result', `<${result}>`);
       });
-    }); 
+      
+      library?.requestGalleryImage().then((result) => {
+        console.log('@result', result);
+
+        const uri = result.trim();
+        if (uri === '') {
+          console.log('@uri 못가져옴...');
+          return;
+        }
+        if (uri === 'permission-deny') {
+          console.log('권한 거부됨!');
+          return;
+        }
+        console.log('@uri', uri);
+        setUri(uri);
+
+        console.log('fetch...');
+        fetch(`file://${uri}`).then(res => {
+          return res.blob();
+        }).then((blob) => {
+          console.log('@blob', blob);
+          const name: string = (blob as any)._data.name;
+          const type: string = (blob as any)._data.type;
+          const lastModified: number = (blob as any)._data.lastModified;
+          console.log('@name', name);
+          const file = new File([blob], name, { type, lastModified });
+          console.log('@file', file);
+        }).catch((error) => {
+          console.error('error', error);
+        });
+      }).catch((error) => {
+        console.error(`library?.requestGalleryImage().error`, error);
+      }); 
+    }, 3000);
+      
   }, []);
 
   return (
